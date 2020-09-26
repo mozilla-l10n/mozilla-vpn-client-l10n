7dd8d8b6969fce005e9388b070d41a71cbdfa2c6
Basti <streich.mobile@gmail.com>
2020-09-26 17:35:01 +0200

    add import sh

---
+++ .github/workflows/import.sh
@@ -0,0 +1,6 @@
+#!bin/bash
+/usr/lib/qt5/bin/lupdate vpn/src/src.pro -ts
+
+for filename in vpn/translations/* do 
+    /usr/lib/qt5/bin/lconvert -i filename -o translationFiles/${basename filename}.xlf; 
+done;
