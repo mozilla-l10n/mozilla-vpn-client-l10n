b7c074412265824a8c034d4e1e2effa1d05e0dd8
Basti <streich.mobile@gmail.com>
2020-09-26 22:05:23 +0200

    Better Arg Stuff

--- .github/import.sh
+++ .github/import.sh
@@ -1,8 +1,15 @@
 #!/usr/bin/bash
+
+# Updates QT Translation sources, converts them into xlf
+# Usage ./import.sh /path/to/mozVPN/ /path/to/translateGit
+# 
+# lupdate & lconvert with qml support must be available
+
+
 /usr/lib/qt5/bin/lupdate vpn/src/src.pro -ts
 
-for FILE in vpn/translations/*; do 
+for FILE in $1/translations/*; do 
 
-/usr/lib/qt5/bin/lconvert -i "$FILE" -o translationFiles/"$(basename "$FILE")".xlf
+/usr/lib/qt5/bin/lconvert -i "$FILE" -o $2/"$(basename "$FILE")".xlf
 
 done
