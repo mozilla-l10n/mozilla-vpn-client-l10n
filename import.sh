e95142a2212cc14b76c948b223fd0ff6a996ac11
Basti <streich.mobile@gmail.com>
2020-09-26 18:09:55 +0200

    syntax fix?

--- import.sh
+++ import.sh
@@ -2,5 +2,7 @@
 /usr/lib/qt5/bin/lupdate vpn/src/src.pro -ts
 
 for filename in vpn/translations/* do 
-    /usr/lib/qt5/bin/lconvert -i filename -o translationFiles/${basename filename}.xlf; 
+
+/usr/lib/qt5/bin/lconvert -i filename -o translationFiles/${basename filename}.xlf; 
+
 done
