c0ae471331dd264c9f3c14f12c2c6e78c61680b3
Basti <streich.mobile@gmail.com>
2020-09-26 20:53:37 +0200

    import

--- import.py
+++
@@ -1,9 +0,0 @@
-#!/env/python3
-import os
-
-os.system('git config --global user.name streich.mobile@gmail.com')
-os.system('git config --global user.password 49445128b26fa4b9b1811e1f4dc9e4d9848270a5')
-
-print("Pulling newest Revision of the Project")
-os.system('git clone https://github.com/bakulf/mv')
-
