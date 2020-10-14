973e287af2ea265fd76865fd2aefe67ad08a1043
Francesco Lodolo [:flod] <flodolo@mozilla.com>
2020-10-14 12:08:51 +0200

    Simplify bash script

--- .github/update.sh
+++ .github/update.sh
@@ -1,6 +1,6 @@
 #! /usr/bin/env bash
 
-# Updates QT Translation sources, converts them into xlf
+# Updates QT Translation sources, converts them into XLIFF
 # Usage ./import.sh /path/to/mozVPN/ /path/to/translateGit
 #
 # lupdate & lconvert with qml support must be available
@@ -11,12 +11,12 @@ LCONVERT=$(which lconvert)
 LUPDATE vpn/src/src.pro -ts
 
 for FILE in $1/translations/*; do
-if [ -f "$2/$(basename '$FILE' .ts).xliff" ]; then
-    echo "Updating $2/$(basename '$FILE' .ts).xliff"
-    LCONVERT -i "$FILE" -i $2/"$(basename "$FILE" .ts)".xliff -o $2/"$(basename "$FILE" .ts)".xliff
-else
-    echo "Importing $2/$(basename '$FILE' .ts).xliff"
-    LCONVERT -i "$FILE" -o $2/"$(basename "$FILE" .ts)".xliff
-fi
-
+    OUT_FILE="$2/$(basename '$FILE' .ts).xliff"
+    if [ -f $OUT_FILE ]; then
+        echo "Updating $OUT_FILE"
+        LCONVERT -i "$FILE" -i $OUT_FILE -o $OUT_FILE
+    else
+        echo "Importing $OUT_FILE"
+        LCONVERT -i "$FILE" -o $OUT_FILE
+    fi
 done
