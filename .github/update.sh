18a1b8803957e1a214f4a60b7807429e5f78f65b
Francesco Lodolo [:flod] <flodolo@mozilla.com>
2020-10-14 07:57:14 +0200

    Clean up update scripts
    - Use os.path instead of hardcoding OS specific separators
    - Coding consistency
    - Don't hardcode path to executables in bash script

--- .github/update.sh
+++ .github/update.sh
@@ -1,20 +1,22 @@
-#!/usr/bin/bash
+#! /usr/bin/env bash
 
 # Updates QT Translation sources, converts them into xlf
 # Usage ./import.sh /path/to/mozVPN/ /path/to/translateGit
-# 
+#
 # lupdate & lconvert with qml support must be available
 
+LUPDATE=$(which lupdate)
+LCONVERT=$(which lconvert)
 
-/usr/lib/qt5/bin/lupdate vpn/src/src.pro -ts
+LUPDATE vpn/src/src.pro -ts
 
-for FILE in $1/translations/*; do 
+for FILE in $1/translations/*; do
 if [ -f "$2/$(basename '$FILE' .ts).xlf" ]; then
-    echo "Updateing $2/$(basename '$FILE' .ts).xlf"
-    /usr/lib/qt5/bin/lconvert -i "$FILE" -i $2/"$(basename "$FILE" .ts)".xlf -o $2/"$(basename "$FILE" .ts)".xlf
-else 
+    echo "Updating $2/$(basename '$FILE' .ts).xlf"
+    LCONVERT -i "$FILE" -i $2/"$(basename "$FILE" .ts)".xlf -o $2/"$(basename "$FILE" .ts)".xlf
+else
     echo "Importing $2/$(basename '$FILE' .ts).xlf"
-    /usr/lib/qt5/bin/lconvert -i "$FILE" -o $2/"$(basename "$FILE" .ts)".xlf
+    LCONVERT -i "$FILE" -o $2/"$(basename "$FILE" .ts)".xlf
 fi
 
 done
