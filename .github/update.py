8eac3f83d41faa6d8b53887782813f81fcf55b3a
Sebastian Streich <streich.mobile@gmail.com>
2020-10-14 17:18:37 +0200

    remove unneded prints

--- .github/update.py
+++ .github/update.py
@@ -35,7 +35,6 @@ srcFile = os.path.join(VPN_PROJECT_DIR, 'src', 'src.pro')
 os.system(f'lupdate {srcFile} -ts')
 
 for fileName in os.listdir(os.path.join(VPN_PROJECT_DIR, 'translations')):
-    print(f'Reading {fileName}')
     if (not fileName.endswith('.ts')):
         continue
     filePath = os.path.join(VPN_PROJECT_DIR, 'translations', fileName)
@@ -46,7 +45,6 @@ for fileName in os.listdir(os.path.join(VPN_PROJECT_DIR, 'translations')):
     # Create folder for each locale and convert
     # ts file to /{locale}/mozillavpn.xliff
     if not os.path.exists(outPath):
-        print(f'Create Folder for {locale}')
         os.mkdir(outPath)
 
     outFile = os.path.join(outPath, f'{baseName}.xliff')
