4c3c4fd4326db669a06c65f993e9b79d7e1ee589
Francesco Lodolo [:flod] <flodolo@mozilla.com>
2020-11-30 16:23:54 +0100

    Add license header to Python scripts

--- .github/extract_source_strings.py
+++ .github/extract_source_strings.py
@@ -1,5 +1,8 @@
 #! /usr/bin/env python3
-# Note: this wont work on python 2.7
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
 # This script must be executed at the root of the repository.
 
 from lxml import etree, objectify
--- .github/update_other_locales.py
+++ .github/update_other_locales.py
@@ -1,4 +1,7 @@
 #! /usr/bin/env python3
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
 """
  update_other_locales.py --reference <locale> --path <base_l10n_folder> --xliff <xliff_filename> [optional list of locales]
