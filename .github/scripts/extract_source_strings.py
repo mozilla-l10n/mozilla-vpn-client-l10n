ae13f8e9b78abd49f496b1664007a277ad002bce
Francesco Lodolo [:flod] <flodolo@mozilla.com>
2021-05-14 18:00:19 +0200

    Make it easier to run string extraction locally

--- .github/scripts/extract_source_strings.py
+++ .github/scripts/extract_source_strings.py
@@ -7,52 +7,80 @@
 
 from lxml import etree, objectify
 from translate.misc.xml_helpers import reindent
+import argparse
 import os
 
-vpn_project_folder = "vpn"
-output_folder = "translationFiles"
 
-srcFile = os.path.join(vpn_project_folder, "src", "src.pro")
-os.system(f"lupdate {srcFile} -ts")
-
-filePath = os.path.join(vpn_project_folder, "translations", "mozillavpn_en.ts")
-outFile = os.path.join(output_folder, "en", "mozillavpn.xliff")
-
-# Update English XLIFF file
-print(f"Updating {outFile}")
-os.system(f"lconvert -if ts -i {filePath} -of xlf -o {outFile}")
-
-# Clean up the new XLIFF file
-NS = {"x": "urn:oasis:names:tc:xliff:document:1.2"}
-tree = etree.parse(outFile)
-root = tree.getroot()
-objectify.deannotate(root, cleanup_namespaces=True)
-
-# Remove targets (i.e. translations) if present, since this is the reference
-# locale
-for target in root.xpath("//x:target", namespaces=NS):
-    if target is not None:
-        target.getparent().remove(target)
-
-# Remove the xml:space attribute in the <source>, since it's not used when
-# exporting back to .ts
-for source in root.xpath("//x:source", namespaces=NS):
-    attrib_name = "{http://www.w3.org/XML/1998/namespace}space"
-    source.attrib.pop(attrib_name)
-
-# Change QT <extracomment> elements into <notes>
-for extracomment in root.xpath("//x:extracomment", namespaces=NS):
-    extracomment.tag = "note"
-
-# Remove all <context-group> elements
-for context_group in root.xpath("//x:context-group", namespaces=NS):
-    context_group.getparent().remove(context_group)
-
-# Replace the existing locale file with the new XML content
-with open(outFile, "w") as fp:
-    # Fix identation of XML file
-    reindent(root)
-    xliff_content = etree.tostring(
-        tree, encoding="UTF-8", xml_declaration=True, pretty_print=True
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--vpn",
+        required=True,
+        dest="vpn_path",
+        help="Path to VPN project repository",
     )
-    fp.write(xliff_content.decode("utf-8"))
+    parser.add_argument(
+        "--l10n",
+        required=True,
+        dest="l10n_path",
+        help="Path to folder including subfolders for all locales",
+    )
+    parser.add_argument(
+        "--lib",
+        required=False,
+        default="",
+        dest="lib_path",
+        help="Path to qt libraries",
+    )
+    args = parser.parse_args()
+
+    source_file = os.path.join(args.vpn_path, "src", "src.pro")
+    exe_path = os.path.join(args.lib_path, "lupdate")
+    os.system(f"{exe_path} {source_file} -ts")
+
+    source_ts_file = os.path.join(args.vpn_path, "translations", "mozillavpn_en.ts")
+    output_xliff_file = os.path.join(args.l10n_path, "en", "mozillavpn.xliff")
+
+    # Update English XLIFF file
+    print(f"Updating {output_xliff_file}")
+    exe_path = os.path.join(args.lib_path, "lconvert")
+    os.system(f"{exe_path} -if ts -i {source_ts_file} -of xlf -o {output_xliff_file}")
+
+    # Clean up the new XLIFF file
+    NS = {"x": "urn:oasis:names:tc:xliff:document:1.2"}
+    tree = etree.parse(output_xliff_file)
+    root = tree.getroot()
+    objectify.deannotate(root, cleanup_namespaces=True)
+
+    # Remove targets (i.e. translations) if present, since this is the reference
+    # locale
+    for target in root.xpath("//x:target", namespaces=NS):
+        if target is not None:
+            target.getparent().remove(target)
+
+    # Remove the xml:space attribute in the <source>, since it's not used when
+    # exporting back to .ts
+    for source in root.xpath("//x:source", namespaces=NS):
+        attrib_name = "{http://www.w3.org/XML/1998/namespace}space"
+        source.attrib.pop(attrib_name)
+
+    # Change QT <extracomment> elements into <notes>
+    for extracomment in root.xpath("//x:extracomment", namespaces=NS):
+        extracomment.tag = "note"
+
+    # Remove all <context-group> elements
+    for context_group in root.xpath("//x:context-group", namespaces=NS):
+        context_group.getparent().remove(context_group)
+
+    # Replace the existing locale file with the new XML content
+    with open(output_xliff_file, "w") as fp:
+        # Fix identation of XML file
+        reindent(root)
+        xliff_content = etree.tostring(
+            tree, encoding="UTF-8", xml_declaration=True, pretty_print=True
+        )
+        fp.write(xliff_content.decode("utf-8"))
+
+
+if __name__ == "__main__":
+    main()
