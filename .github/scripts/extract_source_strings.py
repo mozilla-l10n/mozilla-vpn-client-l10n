ed751d26f9e6b83d83401e436a7d7f74b7185e72
Francesco Lodolo <flod@lodolo.net>
2023-09-14 10:37:21 +0200

    Update rewrite logic

--- .github/scripts/extract_source_strings.py
+++ .github/scripts/extract_source_strings.py
@@ -70,7 +70,8 @@ def main():
             file_name = re.sub(r"^src/", "../src/", file_name)
             file_name = re.sub(r"^nebula/", "../../nebula/", file_name)
             # Changes caused by https://github.com/mozilla-mobile/mozilla-vpn-client/pull/7924
-            file_name = re.sub(r"^../src/", "../src/apps/vpn/", file_name)
+            if not file_name.startswith("../src/apps/vpn/"):
+                file_name = re.sub(r"^../src/", "../src/apps/vpn/", file_name)
             if "i18nstrings_p.cpp" in file_name:
                 file_name = "generated/l18nstrings_p.cpp"
             f.set("original", file_name)
